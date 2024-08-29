76ddfaa5ded08a7c29756e73b9806b0c596b2968
Piyush Prateek <piyushprateek231996@gmail.com>
2024-08-29 14:24:39 +0530

    AppLayout ThemeProvider - added in components Header - added public - img added app.css - custom css added app.tsx - RouterProvider wrapped in ThemeProvider

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -1,10 +1,22 @@
+import Header from '@/components/header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
     return (
-        <div>AppLayout
-            <Outlet />
+        <div>
+            <div className='grid-background'>
+
+            </div>
+
+            <main className=' min-h-screen container'>
+                <Header />
+                <Outlet />
+            </main>
+
+            <div className="p-10 text-center bg-gray-800 mt-10">
+                Made with 💗 by Piyush Prateek
+            </div>
         </div>
     )
 }
