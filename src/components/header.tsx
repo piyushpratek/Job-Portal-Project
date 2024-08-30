a43340484c0bba3dd794c213769dcd108f4b960d
Piyush Prateek <piyushprateek231996@gmail.com>
2024-08-30 13:56:07 +0530

    npm i clerk theme

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
 import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/clerk-react'
