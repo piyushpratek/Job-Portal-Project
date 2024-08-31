bb1f2ec55cd04d4d2792d46ef34c02562f4bfdfc
Piyush Prateek <piyushprateek231996@gmail.com>
2024-08-31 14:21:44 +0530

    protected route - user authentication app - wrapped route in protected route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import PostJob from './pages/post-job'
 import SavedJobs from './pages/saved-job'
 import MyJobs from './pages/my-jobs'
 import { ThemeProvider } from './components/ui/theme-provider'
+import ProtectedRoute from './components/protected-route'
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -19,27 +20,56 @@ const router = createBrowserRouter([
       },
       {
         path: "/onboarding",
-        element: <Onboarding />
+        element:
+          (
+            <ProtectedRoute>
+              <Onboarding />
+            </ProtectedRoute>
+          )
       },
       {
         path: "/jobs",
-        element: <JobListing />
+        element:
+          (
+            <ProtectedRoute>
+              <JobListing />
+            </ProtectedRoute>
+          )
       },
       {
         path: "/job/:id",
-        element: <JobPage />
+        element:
+          (
+            <ProtectedRoute>
+              <JobPage />
+            </ProtectedRoute>
+          )
       },
       {
         path: "/post-job",
-        element: <PostJob />
+        element:
+          (
+            <ProtectedRoute>
+              <PostJob />
+            </ProtectedRoute>
+          )
       },
       {
         path: "/saved-jobs",
-        element: <SavedJobs />
+        element:
+          (
+            <ProtectedRoute>
+              <SavedJobs />
+            </ProtectedRoute>
+          )
       },
       {
         path: "/my-jobs",
-        element: <MyJobs />
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        )
       }
     ]
   }
